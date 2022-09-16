7d498a2f3388e03421b8d0853ef2a36148cd276b
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2022-09-16 16:56:45 +0530

    JavaScript Program to Make a calculator

--- project(15-sep)/program2(calculater)/calculator.js
+++ project(15-sep)/program2(calculater)/calculator.js
@@ -1,3 +1,6 @@
+//javascript file for calculator
+
+//for display button
 function display(val){
 
     document.getElementById('calc').value += val
@@ -5,6 +8,7 @@ function display(val){
     return val
     
     }
+    //for solve button
     function solve(){
     
     let x = document.getElementById('calc').value
@@ -16,11 +20,13 @@ function display(val){
     return y
     
     }
+    //for click button
     function clearScreen(){
     
     document.getElementById('calc').value = ''
     
     }
+    //for back button
     function back() {
       if(value.length>0)
         document.getElementById('calc').value=value.substr(0, value.length - 1);
