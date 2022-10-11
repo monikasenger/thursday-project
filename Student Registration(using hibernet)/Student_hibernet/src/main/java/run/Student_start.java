b3e83fe060d64c2de4462d7bc3553fee69100b43
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2022-10-11 15:49:53 +0530

    Update Student_start.java

--- Student Registration(using hibernet)/Student_hibernet/src/main/java/run/Student_start.java
+++ Student Registration(using hibernet)/Student_hibernet/src/main/java/run/Student_start.java
@@ -3,11 +3,7 @@ package run;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.sql.Date;
-import java.util.List;
-
-import student.Student;
-
+import student_dao.StudentDao;
 import student_dao.StudentDaoImpl;
 
 
@@ -15,13 +11,15 @@ import student_dao.StudentDaoImpl;
 
 public class Student_start extends StudentDaoImpl
 {
-	static String firstname,lastname,date,email,age,gender;
+	//globally declared
 	static int ch,choice;
-	static Student student =null;
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	
+	
+	/******************************************************/
+	//main method
     public static void main( String[] args )
     {
-    	StudentDaoImpl dao = new StudentDaoImpl();
-    	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         while(true) {
         	try {
                 System.out.println("=============REGISTRATIONS FORM==============");
@@ -37,187 +35,30 @@ public class Student_start extends StudentDaoImpl
                 switch(choice) {
         	        case 1://insert data
         	        {
-        	        	System.out.print("Enter the firstname: ");
-        	        	 firstname = input.readLine().trim();
-        	        	System.out.print("Enter the lastname: ");
-        	        	 lastname = input.readLine().trim();
-        	        	System.out.println();
-        	        	System.out.println("Dob format will be yyyy-mm-dd");
-        	        	System.out.print("Enter the dob: ");
-        	        	 date = input.readLine().trim();
-        	        	System.out.print("Enter the age: ");
-        	         age = input.readLine().trim();
-        	        	System.out.print("Enter the gender: ");
-        	        	 gender = input.readLine().trim();
-        	        	System.out.print("Enter the email: ");
-        	        	 email = input.readLine().trim();
-        	         student = new Student(firstname, lastname,date,age,gender,email);
-        	        	System.out.println("\nAdding the user.........");
-        	        	dao.saveStudent(student);
-        	        	System.out.println("Student added successfully!");
+        	        	StudentDao.insertdata();
         	        	break;
         	        }
         	        case 2:
         	        {//search data
-        	        	System.out.print("Enter the studentId: ");
-        	        	long id = Long.valueOf(input.readLine());
-        	        	System.out.println(dao.getStudentById(id));
+        	        StudentDao.searchdata();
         	        	break;
         	        }
         	        case 3:
         	        	//show all
         	        {
-        	        	List<Student> l1 = dao.getAllStudent();
-        	        System.out.println("**=============**=================**=============**");
-        			l1.forEach(ob5 -> System.out.println(ob5.getId()+" -id data:\n"+ob5.getFirstname()+", "+ ob5.getLastname()+", "
-        					+ ob5.getGender()+", "+ ob5.getAge()+", "
-        					+ ob5.getDob()+", "+ ob5.getEmail()));
-        			 System.out.println("**=============**=================**=============**");
+        	        	 StudentDao.showall();
         	        	break;
         	        }
         	        case 4:
         	        {
         	        	//update
-        	        	 System.out.println("1. update first name of student ");
-        	                System.out.println("2. update last name of student");
-        	                System.out.println("3. update age of student");
-        	                System.out.println("4. update gender of student");
-        	                System.out.println("5. update email of student");
-        	                System.out.println("6. update DOB of student");
-        	                System.out.println("7. exit");
-        	                System.out.print("Enter your choice: ");
-        	               ch = Integer.valueOf(input.readLine());
-     
-        	               switch(ch) {
-               	        case 1:   
-               	        {
-               	        	//update first name
-        	        	System.out.println("Enter the studentId: ");
-        	        	long id = Long.valueOf(input.readLine());
-        	        	
-        	        	Student student = dao.getStudentById(id);
-        	        	if(student == null) {
-        	        		System.out.println("Sorry! The student does not exit.");
-        	        		break;
-        	        	}
-        	        	System.out.print("Enter the firstname: ");
-        	        	String firstname = input.readLine().trim();
-               	        
-        	        	if(firstname != ""){
-        	        		student.setFirstname(firstname);
-        	        	 System.out.println("\nUpdating the student.........");
-         	        	dao.updateStudent(student);
-         	        	System.out.println("student updated successfully!");
-        	        	}
-               	        }
-               	        break;
-               	        case 2:{
-               	        	//update last name
-            	        	System.out.println("Enter the studentId: ");
-            	        	long id = Long.valueOf(input.readLine());
-            	        	
-            	        	Student student = dao.getStudentById(id);
-            	        	if(student == null) {
-            	        		System.out.println("Sorry! The student does not exit.");
-            	        		break;
-            	        	}
-               	        System.out.print("Enter the lastname: ");
-        	        	String lastname = input.readLine().trim();
-        	        	if(lastname != "")
-        	        		student.setLastname(lastname);
-        	        	 System.out.println("\nUpdating the student.........");
-         	        	dao.updateStudent(student);
-         	        	System.out.println("student updated successfully!");
-               	        }
-               	        break;
-               	        case 3:
-               	        {
-               	       //update age
-            	        	System.out.println("Enter the studentId: ");
-            	        	long id = Long.valueOf(input.readLine());
-            	        	
-            	        	Student student = dao.getStudentById(id);
-            	        	if(student == null) {
-            	        		System.out.println("Sorry! The student does not exit.");
-            	        		break;
-            	        	}
-        	        	System.out.print("Enter the age: ");
-        	        	String age = input.readLine().trim();
-        	        	if(age != "")
-        	        		student.setAge(age);
-        	        	 System.out.println("\nUpdating the student.........");
-         	        	dao.updateStudent(student);
-         	        	System.out.println("student updated successfully!");
-        	        	}
-               	        break;
-               	        case 4:
-               	        {
-               	       //update gender
-            	        	System.out.println("Enter the studentId: ");
-            	        	long id = Long.valueOf(input.readLine());
-            	        	
-            	        	Student student = dao.getStudentById(id);
-            	        	if(student == null) {
-            	        		System.out.println("Sorry! The student does not exit.");
-            	        		break;
-            	        	}
-        	        	System.out.print("Enter the gender: ");
-        	        	String gender = input.readLine().trim();
-        	        	if(gender != "")
-        	        		student.setGender(gender);
-       	        	 System.out.println("\nUpdating the student.........");
-        	        	dao.updateStudent(student);
-        	        	System.out.println("student updated successfully!");
-        	        	}
-               	     break;
-               	        case 5:{
-               	       //update email
-            	        	System.out.println("Enter the studentId: ");
-            	        	long id = Long.valueOf(input.readLine());
-            	        	
-            	        	Student student = dao.getStudentById(id);
-            	        	if(student == null) {
-            	        		System.out.println("Sorry! The student does not exit.");
-            	        		break;
-            	        	}
-        	        	System.out.print("Enter the email: ");
-        	        	String email = input.readLine().trim();
-        	        	if(email != "")
-        	        		student.setGender(email);
-        	        	}
-               	     break;
-               	        case 6:
-               	        {
-               	       //update dob
-            	        	System.out.println("Enter the studentId: ");
-            	        	long id = Long.valueOf(input.readLine());
-            	        	
-            	        	Student student = dao.getStudentById(id);
-            	        	if(student == null) {
-            	        		System.out.println("Sorry! The student does not exit.");
-            	        		break;
-            	        	}
-        	        	System.out.print("Enter the DOB: ");
-        	        	String DOB = input.readLine().trim();
-        	        	if(DOB != "")
-        	        		student.setGender(DOB);
-        	        	}
-               	       
-        	        break;
-        	        case 7:{//exit
-        	        	System.exit(0);
-        	        }
-        	        }
+        	        	 StudentDao.update();
         	               }
         	       break; 
         	        case 5:
         	        {
         	        	//delete 
-        	        	System.out.println("Enter the userId: ");
-        	        	long id = Long.valueOf(input.readLine());
-        	        	System.out.println("Deleting the user.......");
-        	        	dao.deleteStudentById(id);
-        	        	System.out.println("User deleted successfully!");
+        	        	 StudentDao.delete();
         	        	break;
         	        }
         	        case 6://exit
